4df11da2c37c3b3f0e84de1c3645970cded406dd
Huong, nguyen <thi.huong.nguyen@draphony.com>
2021-10-15 08:24:17 +0700

    .

--- src/components/RouterWithPaths.js
+++ src/components/RouterWithPaths.js
@@ -2,14 +2,13 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 const RouterWithPaths = ({ path = "", roles = [], ...rest }) => {
   return (
-    // <Switch key={rest.key}>
-    //   {typeof path === "string" ? (
-    //     <Route path={path} {...rest} />
-    //   ) : (
-    //     path.map((item, index) => <Route path={item} {...rest} key={index}  />)
-    //   )}
-    // </Switch>
-    <Route path={path} {...rest} />
+    <Switch key={rest.key}>
+      {typeof path === "string" ? (
+        <Route path={path} {...rest} />
+      ) : (
+        path.map((item, index) => <Route path={item} {...rest} key={index} />)
+      )}
+    </Switch>
   );
 };
 
